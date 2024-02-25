cd5220f8d2cfeba66d43374181b7116eb3fe2d03
NahomKibreab <nknahom@gmail.com>
2024-02-25 10:50:12 -0700

    Update logs

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const doc = document.getElementById("root");
 
 const data = () => "hello world";
 
-async function callApi() {
+function callApiWithoutAwait() {
+  // API call using .then() method
+  fetch("https://api.github.com/users/nahomkibreab")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.login);
+    });
+}
+
+async function callApiUsingAsyncAwait() {
+  // API call using async/await
   const rawData = await fetch("https://api.github.com/users/nahomkibreab");
   const data = await rawData.json();
 
@@ -20,6 +30,7 @@ async function callApi() {
 // before calling the api
 console.log("1. before calling the api");
 
-callApi(); // skip this line
+callApiWithoutAwait(); // skip this line because it's a promise function
+callApiUsingAsyncAwait(); // skip this line becayse it's an promise function
 
 console.log("2. after calling the api");
